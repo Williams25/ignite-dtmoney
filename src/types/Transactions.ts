f317fae834508663e962776d23d20b96029b0bf9
Williams25 <william.souza.timao@hotmail.com>
2022-07-02 09:42:28 -0300

    ajustando tipagem

--- src/types/Transactions.ts
+++ src/types/Transactions.ts
@@ -1,8 +1,19 @@
+import { TypeTransition } from "../components/NewTransactionModal";
+
 export type Transactions = {
-  id: number;
+  transactions: {
+    id: number;
+    title: string;
+    amount: number;
+    type: "deposit" | "withdraw";
+    category: string;
+    createdAt: Date;
+  }[];
+};
+
+export type CreateTransactions = {
+  type: TypeTransition | null;
   title: string;
-  amount: number;
-  type: "deposit" | "withdraw";
   category: string;
-  createdAt: Date;
+  value: number;
 };
