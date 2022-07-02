6f589410514dc336af015b955e91ab37c7bc5acf
Williams25 <william.souza.timao@hotmail.com>
2022-07-02 09:42:58 -0300

    criando models transaction

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,35 @@ import { GlobalStyle } from "./styles/global";
 import { ThemeProvider } from "styled-components";
 import App from "./App";
 import theme from "./styles/theme";
-import { createServer } from "miragejs";
-import { TransactionsSerices } from "./services/transactions-service";
+import { createServer, Model } from "miragejs";
 
 createServer({
+  models: {
+    transaction: Model,
+  },
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Freelace de web site",
+          type: "deposit",
+          category: "dev",
+          amount: 6000,
+          createdAt: new Date("2021-02-12"),
+        },
+      ],
+    });
+  },
   routes() {
     this.namespace = "api";
 
-    this.get("/transactions", () => TransactionsSerices.getAll());
+    this.get("/transactions", () => this.schema.all("transaction"));
+    this.post("/transactions", (schema, req) => {
+      const data = JSON.parse(req.requestBody);
+
+      return schema.create("transaction", data);
+    });
   },
 });
 
