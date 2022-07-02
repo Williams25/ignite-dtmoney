a2444cb906661aac4d88ec7463801ccaf6d03dcc
Williams25 <william.souza.timao@hotmail.com>
2022-07-02 09:42:41 -0300

    renderizando modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
+import { useState } from "react";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { TransactionsTable } from "./components/TransactionsTable";
+import Modal from "react-modal";
+import { NewTransactionModal } from "./components/NewTransactionModal";
+
+Modal.setAppElement("#root");
 
 export const App = () => {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState<boolean>(false);
+
+  const toggleOpenOrCloseNewTransactionModal = () =>
+    setIsNewTransactionModalOpen((open) => !open);
+
   return (
     <>
-      <Header />
+      <Header
+        toggleOpenOrCloseNewTransactionModal={
+          toggleOpenOrCloseNewTransactionModal
+        }
+      />
       <Dashboard />
       <TransactionsTable />
+
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={toggleOpenOrCloseNewTransactionModal}
+      />
     </>
   );
 };
