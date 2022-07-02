f1d8b9f700697fd3dfc18dcd473212e9a8eb0214
Williams25 <william.souza.timao@hotmail.com>
2022-07-02 11:25:18 -0300

    ajustando a criação da transação

--- src/services/transactions-service.ts
+++ src/services/transactions-service.ts
@@ -1,12 +1,21 @@
-import { AxiosResponse } from "axios";
 import { CreateTransactions, Transactions } from "../types/Transactions";
+
+import { AxiosResponse } from "axios";
 import { http } from "./axios-config";
 
 export const TransactionsSerices = {
-  getAll: (): Promise<AxiosResponse<Transactions>> => {
-    return http.get<Transactions>(`/transactions`);
+  getAll: (): Promise<AxiosResponse<{ transactions: Transactions[] }>> => {
+    return http.get<{ transactions: Transactions[] }>(`/transactions`);
   },
   create: (data: CreateTransactions) => {
-    return http.post("/transactions", data);
+    const transaction: Transactions = {
+      amount: data.value,
+      category: data.category,
+      createdAt: new Date(),
+      title: data.title,
+      type: data.type!,
+    };
+
+    return http.post("/transactions", transaction);
   },
 };
